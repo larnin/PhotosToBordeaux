2834191c8a5a64e286fa77ab18dcbe1d6ca785a5
Nicolas <nicolas-donald.laurent@laposte.net>
2018-06-26 20:19:10 +0200

    Map generator

---
+++ Assets/Scripts/GeneratorLogic.cs
@@ -0,0 +1,239 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using NRand;
+
+public class GeneratorLogic : MonoBehaviour
+{
+    enum Biome
+    {
+        Sea = 0,
+        Water = 1,
+        Sand = 2,
+        Land = 3,
+        Forest = 4
+    }
+
+    enum TileType
+    {
+        Full = 0,
+        Side = 1,
+        SideDown = 2,
+        Corner = 3,
+        InterCorner = 4
+    }
+
+    [Serializable]
+    class BiomeTiles
+    {
+        [SerializeField] List<GameObject> m_fullObj;
+        [SerializeField] List<GameObject> m_sideObj;
+        [SerializeField] List<GameObject> m_sideDownObj;
+        [SerializeField] List<GameObject> m_cornerObj;
+        [SerializeField] List<GameObject> m_interCornerObj;
+
+        public List<GameObject> get(TileType t)
+        {
+            switch(t)
+            {
+                case TileType.Full:
+                    return m_fullObj;
+                case TileType.Side:
+                    return m_sideObj;
+                case TileType.SideDown:
+                    return m_sideDownObj;
+                case TileType.Corner:
+                    return m_cornerObj;
+                case TileType.InterCorner:
+                    return m_interCornerObj;
+                default:
+                    return null;
+            }
+        }
+
+        public GameObject getOne(TileType t, IRandomGenerator gen)
+        {
+            var biomes = get(t);
+            if (biomes == null || biomes.Count == 0)
+                return null;
+            return biomes[new UniformIntDistribution(biomes.Count - 1).Next(gen)];
+        }
+    }
+
+    [Serializable]
+    class Biomes
+    {
+        [SerializeField] BiomeTiles m_seaTiles;
+        [SerializeField] BiomeTiles m_waterTiles;
+        [SerializeField] BiomeTiles m_sandTiles;
+        [SerializeField] BiomeTiles m_landTiles;
+        [SerializeField] BiomeTiles m_forestTiles;
+
+        public BiomeTiles get(Biome b)
+        {
+            switch(b)
+            {
+                case Biome.Sea:
+                    return m_seaTiles;
+                case Biome.Water:
+                    return m_waterTiles;
+                case Biome.Sand:
+                    return m_sandTiles;
+                case Biome.Land:
+                    return m_landTiles;
+                case Biome.Forest:
+                    return m_forestTiles;
+                default:
+                    return null;
+            }
+        }
+    }
+
+    [Serializable]
+    class GeneratorProperties
+    {
+        public Transform zoneParent;
+        public int width;
+        public int height;
+        public float perlinElevationScale;
+        public float perlinElevationScale2;
+        public float perlinElevationScale3;
+        public float perlinElevationAmplitude2;
+        public float perlinElevationAmplitude3;
+        public float waterHeight;
+        public float OceanHeight;
+        public float perlinForestScale;
+        public float perlinForestScale2;
+        public float perlinForestAmplitude2;
+        public float forestMinHeight;
+        public float forestValue;
+        public float perlinSandScale;
+        public float sandMaxHeight;
+        public float sandValue;
+    }
+
+    const float perlinRange = 1000000;
+
+    [SerializeField] GeneratorProperties m_properties;
+    [SerializeField] Biomes m_biomes;
+
+    void Start()
+    {
+        generate((uint)new StaticRandomGenerator<DefaultRandomGenerator>().Next());
+    }
+    
+    void generate(uint seed)
+    {
+        MT19937 generator = new MT19937(seed);
+
+        var elevation = generateElevation(generator);
+        var tiles = generateBiomes(elevation, generator);
+
+        draw(tiles, generator);
+    }
+
+    Vector2 generateOffset(float range, IRandomGenerator gen)
+    {
+        return new UniformVector2SquareDistribution(-range, range, -range, range).Next(gen);
+    }
+
+    Matrix<float> generateElevation(IRandomGenerator gen)
+    {
+        var perlinOffset = generateOffset(perlinRange, gen);
+        var perlin2Offset = generateOffset(perlinRange, gen);
+        var perlin3Offset = generateOffset(perlinRange, gen);
+
+        Matrix<float> elevation = new Matrix<float>(m_properties.width, m_properties.height, 0);
+
+        for(int i = 0; i < m_properties.width; i++)
+            for(int j = 0; j < m_properties.height; j++)
+            {
+                elevation.set(i, j, Mathf.PerlinNoise(i * m_properties.perlinElevationScale + perlinOffset.x, j * m_properties.perlinElevationScale + perlinOffset.y) * 2 - 1
+                                  + (Mathf.PerlinNoise(i * m_properties.perlinElevationScale2 + perlin2Offset.x, j * m_properties.perlinElevationScale2 + perlin2Offset.y) * 2 - 1) * m_properties.perlinElevationAmplitude2
+                                  + (Mathf.PerlinNoise(i * m_properties.perlinElevationScale3 + perlin3Offset.x, j * m_properties.perlinElevationScale3 + perlin3Offset.y) * 2 - 1) * m_properties.perlinElevationAmplitude3);
+            }
+        return elevation;
+    }
+
+    Matrix<Biome> generateBiomes(Matrix<float> elevation, IRandomGenerator gen)
+    {
+        Matrix<Biome> tiles = new Matrix<Biome>(m_properties.width, m_properties.height, Biome.Sea);
+
+        var perlinOffset = generateOffset(perlinRange, gen);
+        var perlin2Offset = generateOffset(perlinRange, gen);
+        var perlin3Offset = generateOffset(perlinRange, gen);
+
+        for (int i = 0; i < m_properties.width; i++)
+            for (int j = 0; j < m_properties.height; j++)
+            {
+                float height = elevation.get(i, j);
+                float forestValue = Mathf.PerlinNoise(i * m_properties.perlinForestScale + perlinOffset.x, j * m_properties.perlinForestScale + perlinOffset.y) * 2 - 1
+                                 + (Mathf.PerlinNoise(i * m_properties.perlinForestScale2 + perlin2Offset.x, j * m_properties.perlinForestScale2 + perlin2Offset.y) * 2 - 1) * m_properties.perlinForestAmplitude2;
+                float sandValue = Mathf.PerlinNoise(i * m_properties.perlinSandScale + perlin3Offset.x, j * m_properties.perlinSandScale + perlin3Offset.y) * 2 - 1;
+
+                if (height < m_properties.OceanHeight)
+                    tiles.set(i, j, Biome.Sea);
+                else if (height < m_properties.waterHeight)
+                    tiles.set(i, j, Biome.Water);
+                else if (height < m_properties.sandMaxHeight && sandValue > m_properties.sandValue)
+                    tiles.set(i, j, Biome.Sand);
+                else if (height > m_properties.forestMinHeight && forestValue > m_properties.forestValue)
+                    tiles.set(i, j, Biome.Forest);
+                else tiles.set(i, j, Biome.Land);
+            }
+
+        return tiles;
+    }
+
+    void drawPlaceHolder(Matrix<Biome> tiles, IRandomGenerator gen)
+    {
+        for (int i = 0; i < m_properties.width; i++)
+            for (int j = 0; j < m_properties.height; j++)
+            {
+                var prefab = m_biomes.get(tiles.get(i, j)).getOne(TileType.Full, gen);
+                var obj = Instantiate(prefab, m_properties.zoneParent);
+                obj.transform.localPosition = new Vector3(i, 0, j);
+            }
+    }
+
+    void draw(Matrix<Biome> tiles, IRandomGenerator gen)
+    {
+        for (int i = 0; i < m_properties.width; i++)
+            for (int j = 0; j < m_properties.height; j++)
+            {
+                var local = tiles.getLocal(i, j, tiles.get(i, j));
+
+                for(int b = 0; b <= (int) local.get(1, 1); b++)
+                {
+                    Biome biome = (Biome)b;
+                    if (!local.have(biome))
+                        continue;
+                    drawCorner(i, j, local.get(0, 1), local.get(1, 0), local.get(0, 0), biome, 0, gen);
+                    drawCorner(i, j, local.get(1, 0), local.get(2, 1), local.get(2, 0), biome, -90, gen);
+                    drawCorner(i, j, local.get(2, 1), local.get(1, 2), local.get(2, 2), biome, -180, gen);
+                    drawCorner(i, j, local.get(1, 2), local.get(0, 1), local.get(0, 2), biome, -270, gen);
+                }
+            }
+    }
+    
+    void drawCorner(int x, int y, Biome left, Biome top, Biome topLeft, Biome current, float rot, IRandomGenerator gen)
+    {
+        TileType type = TileType.Full;
+
+        if (left >= current && top >= current && topLeft >= current)
+            type = TileType.Full;
+        else if (left >= current && top >= current)
+            type = TileType.InterCorner;
+        else if (top >= current)
+            type = TileType.Side;
+        else if (left >= current)
+            type = TileType.SideDown;
+        else type = TileType.Corner;
+
+        var prefab = m_biomes.get(current).getOne(type, gen);
+        var obj = Instantiate(prefab, m_properties.zoneParent);
+        obj.transform.localPosition = new Vector3(2*x, 0, 2*y);
+        obj.transform.Rotate(new Vector3(0, rot));
+    }
+}
---
+++ Assets/Scripts/Matrix.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class Matrix<T>
+{
+    List<T> m_values = new List<T>();
+    public int width { get; private set; }
+    public int height { get; private set; }
+
+    public Matrix(int _width, int _height, T value = default(T))
+    {
+        for (int i = 0; i < _width; i++)
+            for (int j = 0; j < _height; j++)
+                m_values.Add(value);
+
+        width = _width;
+        height = _height;
+    }
+
+    public T get(int x, int y)
+    {
+        return m_values[x + y * width];
+    }
+
+    public void set(int x, int y, T value)
+    {
+        m_values[x + y * width] = value;
+    }
+
+    public Matrix<T> getLocal(int x, int y, T value = default(T))
+    {
+        Matrix<T> mat = new Matrix<T>(3, 3, value);
+
+        for(int i = -1; i <= 1; i++)
+            for(int j = -1; j <= 1; j++)
+            {
+                if (x + i < 0 || y + j < 0 || x + i >= width || y + j >= height)
+                    continue;
+                mat.set(i + 1, j + 1, get(x + i, y + j));
+            }
+
+        return mat;
+    }
+
+    public bool have(T value)
+    {
+        foreach (var it in m_values)
+            if (EqualityComparer<T>.Default.Equals(it, value))
+                return true;
+        return false;
+    }
+}
